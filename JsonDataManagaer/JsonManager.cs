8baac8daa19cec7192c9a130363223b0221cbf29
Pavan699 <pavannakate03@gmail.com>
2021-09-14 10:45:03 -0700

    Adding Bill file of items

--- JsonDataManagaer/JsonManager.cs
+++ JsonDataManagaer/JsonManager.cs
@@ -11,13 +11,21 @@ namespace JsonDataManagaer
         public string name { get; set; }
         public double weigth { get; set; }
         public double price { get; set; }
-    }  
-    
+    }
+    class Result
+    {
+        public string name { get; set; }
+        public double weigth { get; set; }
+        public double price { get; set; }
+        public double bill { get; set; }
+    }
     public class JsonManager
     {
         //Lists to store the data(created Objects)...
         List<User> writeuser = new List<User>();
-        
+        List<User> readuser = new List<User>();
+
+        List<Result> Billuser = new List<Result>();
         public void calculate()
         {
             User us1 = new User()//data assining
@@ -45,10 +53,37 @@ namespace JsonDataManagaer
             string write = JsonConvert.SerializeObject(writeuser);
             File.WriteAllText(@"D:\VS .net\JsonDataManagaer\JsonDataManagaer\Details.json", write);//fileCreation and Printing the string format data
             //printing the data from Object
+            Console.WriteLine("Details : Name - Weigth - Price");
             foreach (var i in writeuser)
             {
                 Console.WriteLine(i.name + "    " + i.weigth + "    " + i.price);
-            }          
+            }
+
+            //Reading the file and storing the data in a string value
+            string read = File.ReadAllText(@"D:\VS .net\JsonDataManagaer\JsonDataManagaer\Details.json");
+            readuser = JsonConvert.DeserializeObject<List<User>>(read);//Conversion of string into Object
+
+            Console.WriteLine();
+            //Printing the Objects
+            foreach (User i in readuser)
+            {
+                Result bui = new Result()
+                {
+                    name = i.name,
+                    weigth = i.weigth,
+                    price = i.price,
+                    bill = i.price * i.weigth
+                };
+                Billuser.Add(bui);
+            }
+            string jsonbill = JsonConvert.SerializeObject(Billuser);
+            File.WriteAllText(@"D:\VS .net\JsonDataManagaer\JsonDataManagaer\Bill.json", jsonbill);
+            Console.WriteLine("Details : Name - Weigth - Price - Bill");
+            foreach (var i in Billuser)
+            {
+                Console.WriteLine(i.name + "    " + i.weigth + "    " + i.price  + "   " +i.bill );
+            }
+            Console.ReadKey();
         }
     }
 }
